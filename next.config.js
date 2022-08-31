9d31038d8557d52648f603028ce9f2046a15068d
ImJustChew <chewtzihwee@gmail.com>
2022-08-31 19:19:05 +0800

    god please fix yourself

--- next.config.js
+++ next.config.js
@@ -15,8 +15,6 @@ module.exports = {
   images: {
     domains: ['img.icons8.com'],
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   experimental: {
     images: {
       unoptimized: true //Don't Optimize Images if we're on static hosting
