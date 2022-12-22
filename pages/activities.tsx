9c3acd0d8f1b2efee964046d54affb6449be9c0e
Boon <34598848+bjun626@users.noreply.github.com>
2022-12-22 21:59:08 +0800

    Added docs link

--- pages/activities.tsx
+++ pages/activities.tsx
@@ -1,13 +1,17 @@
-import { NextPage } from "next";
-import Layout from "../components/Layout";
+import { NextPage } from "next"
+import Link from "next/link"
+import Layout from "../components/Layout"
 
 const Activities: NextPage = () => {
-    return (
-        <Layout title="Activities">
-            <h1 className='text-4xl font-bold py-4 text-center'>Activities</h1>
-            <p>Coming Soon...</p>
-        </Layout>
-    )
+  return (
+    <Layout title="Activities">
+      <h1 className="text-4xl font-bold py-4 text-center">Activities</h1>
+      <div className="text-4xl text-blue-600 mb-2">Documentation</div>
+      <Link href="https://docs.ceag.cc/" legacyBehavior>
+        <div className="underline hover:font-bold">Link</div>
+      </Link>
+    </Layout>
+  )
 }
 
-export default Activities;
+export default Activities
